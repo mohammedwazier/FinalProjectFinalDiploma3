331ceef2cc490eb1c28288e21c043c5bb2d01bfe
MohammedWazier <m.waziruddin@gmail.com>
2019-05-10 01:52:49 +0700

    Adding auto deploy script when deployment stage

--- maintenance/maintenance.js
+++ maintenance/maintenance.js
@@ -2,19 +2,24 @@ const express = require("express");
 const router = express.Router();
 const http = require("http");
 const app = express();
+const path = require("path");
 
 const server = http.createServer(app);
 
-const port = 5000;
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "index.html"));
+// });
 
 app.use(
   "*",
   router.get("", (req, res) => {
     console.log("hehehe");
-    res.send("Hello Worlds!");
+    res.sendFile(path.join(__dirname, "index.html"));
   })
 );
 
+const port = 5000;
+
 server.listen(port, () => {
-  console.log("Server is running on Port http://localhost:" + port);
+  console.log("Server maintenance is running on Port http://localhost:" + port);
 });
