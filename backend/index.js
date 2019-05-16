28b28b7cdff1c5db0f18e689bb12e56ebf658e09
mohammedwazier <m.waziruddin@gmail.com>
2019-05-16 08:55:09 +0700

    Update

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,9 @@ const express = require("express");
 const router = express.Router();
 const http = require("http");
 const app = express();
+const bodyParser = require("body-parser");
 
-const server = http.createServer(app);
-
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(
   "*",
@@ -15,18 +14,65 @@ app.use(
   })
 );
 
-server.listen(port, () => {
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  req.setTimeout(0);
+  next();
+});
+app.use(
+  bodyParser.json({
+    limit: "50mb"
+  })
+);
+app.use(
+  bodyParser.json({
+    type: function(req) {
+      return req.headers["content-type"] === "*/*";
+      server.timeout = 1000;
+    }
+  })
+);
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
+let httpServer = http.createServer(app);
+let server = httpServer.listen(port, () => {
   console.log("Server is running on Port http://localhost:" + port);
 });
 
+// Socket IO Interface
+
 const io = require("socket.io")(server);
+
+var list_user = 0;
 
 io.on("connection", socket => {
   list_user++;
   console.log("new user connected, List User :  " + list_user);
+
+  socket.on("login", data => {
+    var clientInfo = new Object();
+    clientInfo.username = data.username;
+    clientInfo.clientId = socket.id;
+
+    //Update to mongoDB
+  });
   socket.on("disconnect", function() {
     console.log("disconnected");
     list_user--;
     console.log("new user connected, List User :  " + list_user);
   });
 });
+
+// End Socket IO Interface
