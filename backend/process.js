da76bea3e40c2dbf211338ac48c5f4fe6558baae
mohammedwazier <m.waziruddin@gmail.com>
2019-07-02 21:01:37 +0700

    update Backend

--- backend/process.js
+++ backend/process.js
@@ -128,8 +128,8 @@ module.exports = class process {
             try {
                 db.collection(collection)
                     .find({ [param]: value })
-                    .limit(limit)
                     .sort({ $natural: -1 })
+                    .limit(limit)
                     .toArray((err, resp) => {
                         resolve(resp);
                     });
